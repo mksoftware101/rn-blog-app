d845bb90e887f437e75c2e180b349db088f26144
Marek Kaszycki <marek.kaszycki@northmill.se>
2021-12-03 16:02:58 +0100

    Use useReducer hook insted of useState

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,16 +1,27 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 // Context was designed for moving information around
 
 const BlogContext = React.createContext([]);
 
+// blogPosts = state (state is the name uses for conventions)
+const blogReducer = (blogPosts, action) => {
+    switch (action.type) {
+        case 'add_blogpost': {
+            return [...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}]
+        }
+        default :
+            return blogPosts;
+    }
+}
+
 export const BlogProvider = ({children}) => {
-    const [blogPosts, setBlogPosts] = useState([])
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
     const addBlogPost = () => {
-        setBlogPosts([...blogPosts, {title: `Blog Post #${blogPosts.length + 1}`}])
-    }
+        dispatch({type: 'add_blogpost'});
+    };
 
-    return <BlogContext.Provider value={{data: blogPosts, addBlogPost: addBlogPost}}>
+    return <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
         {children}
     </BlogContext.Provider>
 };
