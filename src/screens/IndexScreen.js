f22604070ca68946fddeead89525f06569b70cd9
Marek Kaszycki <marek.kaszycki@northmill.se>
2021-12-13 13:50:43 +0100

    Styling a little bit"

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,6 +1,14 @@
 import React, {useContext} from 'react';
+import {Context /* as BlogContext (if we have second or more lines of contexts */} from "../context/BlogContext";
 import {View, Text, StyleSheet, FlatList, Button} from "react-native";
-import { Context /* as BlogContext (if we have second or more lines of contexts */ } from "../context/BlogContext";
+import {AntDesign} from '@expo/vector-icons';
+
+const renderItem = ({item}) => {
+    return <View style={styles.row}>
+        <Text style={styles.title}>{item.title}</Text>
+        <AntDesign style={styles.icon} name="delete"/>
+    </View>
+}
 
 const IndexScreen = () => {
     const {state, addBlogPost} = useContext(Context)
@@ -8,12 +16,24 @@ const IndexScreen = () => {
     return <View>
         <Text>IndexScreen</Text>
         <Button title='Add' onPress={addBlogPost}/>
-        <FlatList data={state} renderItem={({item}) => {
-            return <Text>{item.title}</Text>
-        }} keyExtractor={(item => item.title)}/>
+        <FlatList data={state} renderItem={renderItem} keyExtractor={(item => item.title)}/>
     </View>
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        paddingHorizontal: 16,
+        paddingVertical: 12,
+        justifyContent: "space-between",
+    },
+    title: {
+        fontSize: 18,
+    },
+    icon: {
+        fontSize: 24,
+        color: "black",
+    }
+})
 
 export default IndexScreen
