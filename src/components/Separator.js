783b17d79e789a5284626283a3ce394130e2e9a9
Marek Kaszycki <marek.kaszycki@northmill.se>
2021-12-16 15:51:07 +0100

    Fill create screen

---
+++ src/components/Separator.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View} from "react-native";
+
+const Separator = ({ height = 8 }) => {
+    return <View style={{height}}/>
+}
+
+export default Separator
--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, TextInput, Button} from "react-native";
+import Separator from "../components/Separator";
 
 const CreateScreen = () => {
     return <View>
-        <Text>CreateScreen</Text>
+        <Text style={styles.title}>Enter Title:</Text>
+        <Separator/>
+        <TextInput style={styles.input}/>
+        <Separator height={16}/>
+        <Text style={styles.content}>Enter Content:</Text>
+        <Separator/>
+        <TextInput style={styles.input}/>
+        <Separator height={16}/>
+        <Button style={styles.button} title='Save' onPress={() => {
+            console.log('clicked save')
+        }}/>
     </View>
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    title: {
+        marginStart: 16,
+        marginTop: 16,
+    },
+    input: {
+        borderWidth: 1,
+        borderRadius: 5,
+        fontSize: 12,
+        marginStart: 16,
+        marginEnd: 16,
+    },
+    content: {
+        marginStart: 16,
+        marginTop: 16,
+    },
+})
 
 export default CreateScreen
