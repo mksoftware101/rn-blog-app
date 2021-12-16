6c8462f3aaacfcb429873ddebab9766f32348287
Marek Kaszycki <marek.kaszycki@northmill.se>
2021-12-16 15:13:38 +0100

    Show blog posts

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -3,19 +3,24 @@ import {Context /* as BlogContext (if we have second or more lines of contexts *
 import {View, Text, StyleSheet, FlatList, Button, TouchableOpacity} from "react-native";
 import {AntDesign} from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({navigation}) => {
     const {state, addBlogPost, deleteBlogPost} = useContext(Context)
-
     return <View>
         <Text>IndexScreen</Text>
         <Button title='Add' onPress={addBlogPost}/>
         <FlatList data={state} renderItem={({item}) => {
-            return <View style={styles.row}>
-                <Text style={styles.title}>{item.title}</Text>
-                <TouchableOpacity onPress={() => {deleteBlogPost(item.id)}}>
-                    <AntDesign style={styles.icon} name="delete"/>
-                </TouchableOpacity>
-            </View>
+            return <TouchableOpacity onPress={() => {
+                navigation.navigate('Show', {id: item.id})
+            }}>
+                <View style={styles.row}>
+                    <Text style={styles.title}>{item.title}</Text>
+                    <TouchableOpacity onPress={() => {
+                        deleteBlogPost(item.id)
+                    }}>
+                        <AntDesign style={styles.icon} name="delete"/>
+                    </TouchableOpacity>
+                </View>
+            </TouchableOpacity>
         }} keyExtractor={(item => item.title)}/>
     </View>
 }
